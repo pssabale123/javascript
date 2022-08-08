027fb00cec5ced86a20ca020b3b704072c187e70
prashant <pssabale123@gmail.com>
2022-08-08 14:52:25 +0530

    first comment

--- MyWork/demmo.js
+++ MyWork/demmo.js
@@ -1,15 +1,11 @@
+let ob = (a, b, c) => {
+  return {
+    num1: a,
+    num2: b,
+    num3: c,
+  };
+};
 
-let ob = (a,b,c,)=>
-{
-    return {   
-
-    
-         num1:a,
-         num2:b,
-         num3:c 
-        
-    }
-}
-
-let obj = ob(1,2,3);
+let obj = ob(1, 2, 3);
 console.log(obj);
+//first comment
